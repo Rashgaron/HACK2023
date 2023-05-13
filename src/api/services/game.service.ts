ed3a7dffbe675366e4959c971392519498161cb4
Rashgaron <d.rodriguez@fruntera.com>
2023-05-13 12:36:59 +0200

    Fix addPunctuationToGame

--- src/api/services/game.service.ts
+++ src/api/services/game.service.ts
@@ -9,27 +9,30 @@ const gameService = () => {
     try {
       const user = await Users.findById(id);
       if (user) {
-        user.coins = user.coins + punctuation + extraCoins;
-        user.totalPunctuation += Number(punctuation);
-        if (user.maxPunctuations.length < 10) {
-          user.maxPunctuations.push(punctuation);
-        } else {
-          let indexToReplace = -1;
-          let punctuationToReplace = 10000000000000000;
-          user.maxPunctuations.map((maxPunctuation, index) => {
-            if (
-              punctuation > maxPunctuation &&
-              punctuationToReplace > maxPunctuation
-            ) {
-              indexToReplace = index;
-              punctuationToReplace = maxPunctuation;
-            }
-          });
-          if (indexToReplace !== -1)
-            user.maxPunctuations[indexToReplace] = punctuation;
+        user.coins = user.coins + punctuation ?? 0 + extraCoins ?? 0;
+        if (punctuation) {
+          user.totalPunctuation += Number(punctuation);
+          if (user.maxPunctuations.length < 10) {
+            user.maxPunctuations.push(punctuation);
+          } else {
+            let indexToReplace = -1;
+            let punctuationToReplace = 10000000000000000;
+            user.maxPunctuations.map((maxPunctuation, index) => {
+              if (
+                punctuation > maxPunctuation &&
+                punctuationToReplace > maxPunctuation
+              ) {
+                indexToReplace = index;
+                punctuationToReplace = maxPunctuation;
+              }
+            });
+            if (indexToReplace !== -1)
+              user.maxPunctuations[indexToReplace] = punctuation;
+          }
+
+          user.maxPunctuations.sort((a, b) => b - a);
         }
 
-        user.maxPunctuations.sort((a, b) => b - a);
         await user.save();
         return user;
       }
